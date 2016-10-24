a62253bf6f5caf5310c76c1a5b1479ab226a1133
xavilopez <xavi_lopez011@hotmail.com>
2016-10-24 08:59:11 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $("#opt1").on('click', function(){
+    window.location.href='https://xavilopez.github.io/Flyer1/';
+  });
+
+  $("#opt2").on('click', function(){
+    window.location.href='https://xavilopez.github.io/Flyer2/';
+  });
+
+});
